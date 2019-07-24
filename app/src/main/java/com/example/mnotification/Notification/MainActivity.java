b214e93c17eb8901fbba748bd8137f2e9eec099b
Mehul <mehul.excelsior@gmail.com>
2019-07-24 19:12:58 +0530

    change

--- app/src/main/java/com/example/mnotification/Notification/MainActivity.java
+++ app/src/main/java/com/example/mnotification/Notification/MainActivity.java
@@ -131,6 +131,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("*********","-------------");
         ButterKnife.bind(this);
         mFirebaseInstance = FirebaseDatabase.getInstance();
         // get reference to 'users' node
